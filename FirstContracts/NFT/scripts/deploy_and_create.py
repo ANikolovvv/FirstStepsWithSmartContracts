7cc1856d4227ea42217721364b4aca069d6b6b57
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2025-01-26 18:01:55 +0200

    Update deploy_and_create.py

--- FirstContracts/NFT/scripts/deploy_and_create.py
+++ FirstContracts/NFT/scripts/deploy_and_create.py
@@ -3,15 +3,18 @@ from brownie import NFT
 
 
 sample_token_uri = (
-    "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+    "https://ipfs.io/ipfs/QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW?filename=st-bernard.png",
 )
 
 
 def deploy_and_create():
     account = get_account()  
-    nft = NFT.deploy({"from": account})  
+    nft = NFT.deploy({"from": account})
+    tx=nft.createNFT(sample_token_uri,{"from": account})
+    tx.wait(1);  
 
     print(f"NFT contract deployed at: {nft.address}")
+    return nft
 
 
 def main():
