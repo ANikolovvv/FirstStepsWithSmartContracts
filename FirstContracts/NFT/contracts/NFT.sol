de4264abf55c76f2ccad023400a8d76dcb422cd7
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2025-01-27 19:42:55 +0200

    Update NFT contract

--- FirstContracts/NFT/contracts/NFT.sol
+++ FirstContracts/NFT/contracts/NFT.sol
@@ -1,25 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // Импортиране на разширението за URI
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFT is ERC721URIStorage, AccessControl {
     uint256 public tokenCounter;
+    string public baseURI;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC721("Rex", "Dog") {
-        tokenCounter = 0;
+    event NFTMinted(address indexed to, uint256 tokenId, string tokenURI);
+
+    constructor(string memory _baseURI) ERC721("Rex", "DOG") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        baseURI = _baseURI;
     }
 
-    function createNFT(string memory tokenURI) public returns (uint256) {
-        uint256 newTokenId = tokenCounter;
+    function createNFT(string memory tokenURI) public onlyRole(MINTER_ROLE) returns (uint256) {
+        uint256 newTokenId = tokenCounter++;
         _mint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
-        tokenCounter = tokenCounter + 1;
+        _setTokenURI(newTokenId, string(abi.encodePacked(baseURI, tokenURI)));
+        emit NFTMinted(msg.sender, newTokenId, tokenURI);
         return newTokenId;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = _newBaseURI;
     }
 
     function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
         super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
