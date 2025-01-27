1aa0ac3c54dbd06fcef57926b8232a2bcb00da2e
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2025-01-27 19:43:34 +0200

    Update utils.py

--- FirstContracts/NFT/scripts/utils.py
+++ FirstContracts/NFT/scripts/utils.py
@@ -1,18 +1,15 @@
 from brownie import accounts, network, config
 from typing import Optional
 
-
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+LOCAL_BLOCKCHAINS = ["hardhat", "development", "ganache"]
 
 def get_account(index: Optional[int] = None, id: Optional[str] = None):
-  
     if index is not None:
         return accounts[index]
-
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if network.show_active() in LOCAL_BLOCKCHAINS:
         return accounts[0]
-
     if id is not None:
         return accounts.load(id)
-
-    return accounts.add(config["wallets"]["from_key"])
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    raise Exception("No valid account configuration found")
